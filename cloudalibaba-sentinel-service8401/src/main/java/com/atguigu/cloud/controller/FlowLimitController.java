12f5941131be7250c4a348c8f31e6db8c8944aa6
bieyunchao <2813169990@qq.com>
2024-04-23 18:16:12 +0800

    添加/testF

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
@@ -43,4 +43,10 @@ public class FlowLimitController {
         return "------testE";
     }
 
+    @GetMapping("/testF")
+    public String testF() {
+        flowLimitService.common();
+        return "------testF";
+    }
+
 }
